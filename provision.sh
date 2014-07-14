8aef9576f15ed5820ba7b13a0bfe5662ff4c513a
nulltask <nulltask@gmail.com>
2014-07-14 14:55:13 +0900

    fix broken symlink

--- provision.sh
+++ provision.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -x
 
+DIRNAME=$(cd $(dirname $BASH_SOURCE); pwd)
+
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew bundle ./Brewfile
 
 boot2docker init
 boot2docker up
 
-ln -s ./.files/.bash_profile ~/
-ln -s ./.files/.bashrc ~/
+ln -s $DIRNAME/.files/.bash_profile ~/
+ln -s $DIRNAME/.files/.bashrc ~/
 source ~/.bash_profile
 
 git clone git@github.com:drone/drone.git ~/
