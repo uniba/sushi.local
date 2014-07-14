e70325fafe183d083fd70808983664deb7ff6c65
nulltask <nulltask@gmail.com>
2014-07-14 14:30:12 +0900

    symlink .bash{rc,_profile}

--- provision.sh
+++ provision.sh
@@ -3,4 +3,8 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew bundle ./Brewfile
 
+ln -s ./.files/.bash_profile ~/
+ln -s ./.files/.bashrc ~/
+source ~/.bash_profile
+
 git clone git@github.com:drone/drone.git ~/
