6a368c1d79e5da8594ec38f088c2db24127b2fa8
nulltask <nulltask@gmail.com>
2014-07-14 14:41:35 +0900

    add boot2docker

--- .files/.bashrc
+++ .files/.bashrc
@@ -1,2 +1,2 @@
 
-
+export DOCKER_HOST=tcp://192.168.59.103:2375
--- provision.sh
+++ provision.sh
@@ -3,6 +3,9 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew bundle ./Brewfile
 
+boot2docker init
+boot2docker up
+
 ln -s ./.files/.bash_profile ~/
 ln -s ./.files/.bashrc ~/
 source ~/.bash_profile
